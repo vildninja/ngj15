41108991bd855fa7e026b2893c2d83099c03d6c6
Jannek Egeskov Kristensen <jannek.ek@gmail.com>
2015-02-06 23:40:47 +0100

    player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,16 +5,36 @@ public class PlayerController : MonoBehaviour
 {
 
     public string playerPrefix;
+    public float force;
+    public float jumpForce;
 
+    public bool isGrounded;
 
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
-	// Update is called once per frame
-	void Update ()
+
+    void Update()
     {
-	    Vector2 direction = new Vector2(Input.GetAxis(""), 0);
+        if (isGrounded && Input.GetButtonDown(playerPrefix + "Jump"))
+        {
+            rigidbody.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        Vector3 direction = new Vector3(Input.GetAxis(playerPrefix + "Horizontal"), 0, Input.GetAxis(playerPrefix + "Vertical"));
+
+        if (direction.sqrMagnitude > 0.2f)
+        {
+            transform.forward = direction.normalized;
+            //transform.up = Vector3.Cross(rigidbody.velocity, transform.right).normalized;
+            //transform.LookAt(transform.position + rigidbody.velocity);
+        }
+
+        rigidbody.AddForce(direction * force);
 	}
 }
