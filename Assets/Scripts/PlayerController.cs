25a1696288e5b53f5b44bdddcb360f43e5ad4f3d
Jannek Egeskov Kristensen <jannek.ek@gmail.com>
2015-02-06 23:08:50 +0100

    updated git ignore, and started player input

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public string playerPrefix;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    Vector2 direction = new Vector2(Input.GetAxis(""), 0);
+	}
+}
